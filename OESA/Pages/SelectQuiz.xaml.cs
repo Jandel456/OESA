51bb19d160e716db9978b3f201a6d420d8b9acd5
Jandel <jandel456@gmail.com>
2025-04-29 21:09:34 -0400

    Quiz Selection
    I got the Select Quiz page to work, it includes 3 buttons made using C# with a data template from XAML.
    
    The button, when clicked, sends you to quizzes 1, 2 and 3,.
    
    If you were to add more quizzes, lot a like more, the layout will remain logical and not overflow to the right.

--- OESA/Pages/SelectQuiz.xaml.cs
+++ OESA/Pages/SelectQuiz.xaml.cs
@@ -1,9 +1,43 @@
+using System.Windows.Input;
+
 namespace OESA.Pages;
 
 public partial class SelectQuiz : ContentPage
 {
-	public SelectQuiz()
+    public List<ButtonItem> QuizList { get; set; }
+    public SelectQuiz()
 	{
 		InitializeComponent();
-	}
+
+        QuizList = new List<ButtonItem>
+        {
+            new ButtonItem { Title = "Quiz 1: General PHP Knowledge", Description = "Test your knowledge in PHP practices", QuizLocation = new Command(GotoQuiz1) },
+            new ButtonItem { Title = "Quiz 2: PHP Data Types & Arrays", Description = "Test your knowledge in data types and arrays", QuizLocation = new Command(GotoQuiz2) },
+            new ButtonItem { Title = "Quiz 3: PHP & Web Development", Description = "Capstone Quiz", QuizLocation = new Command(GotoQuiz3) }
+        };
+
+        BindingContext = this; // This connects XAML to the data, very odd process?
+
+    }
+    public class ButtonItem
+    {
+        public required string Title { get; set; }      //Title is required
+        public string? Description { get; set; }        // Description is optional, actually kinda cool syntax notation
+        public required ICommand QuizLocation { get; set; }
+
+    }
+
+    private async void GotoQuiz1()
+    {
+        await Navigation.PushAsync(new Quiz1());
+    }
+
+    private async void GotoQuiz2()
+    {
+        await Navigation.PushAsync(new Quiz2());
+    }
+   private async void GotoQuiz3()
+    {
+        await Navigation.PushAsync(new Quiz3());
+    }
 }
